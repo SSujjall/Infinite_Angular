edb5b95024766788c8ebe5e0fd7f5fa391b510de
Sujal Maharjan <suzallmhz@gmail.com>
2025-06-30 10:32:05 +0545

    import bug fix

--- Angular2/src/app/app.ts
+++ Angular2/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { IUser, UserService } from './services/user.service';
 import { User } from './components/user/user';
+import { Users } from './components/users/users';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, User],
+  imports: [RouterOutlet, Navbar, User, Users],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
