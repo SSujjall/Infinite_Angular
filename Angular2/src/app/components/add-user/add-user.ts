b9c86fcd4bef9f0793e00d15bb271e53a1b8d09a
Sujal Maharjan <suzallmhz@gmail.com>
2025-07-02 10:59:54 +0545

    Displaying Errors

--- Angular2/src/app/components/add-user/add-user.ts
+++ Angular2/src/app/components/add-user/add-user.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-user',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './add-user.html',
   styleUrl: './add-user.css'
 })
 export class AddUser {
+
+    usernameFromts = ""
+    imageFromts = ""
+
+
+
   onSubmit(form: any) {
     console.log(form, "form")
   }
